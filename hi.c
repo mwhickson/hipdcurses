a78ad678037557c56015a9e4960ee70f3737f433
Matthew Hickson <matthewhickson@outlook.com>
2021-11-21 00:16:03 -0500

    reorganization for clarity

--- hi.c
+++ hi.c
@@ -3,65 +3,43 @@
 
 /* gcc -o hi.exe hi.c -Wall -lpdcurses -mwindows */
 
-short THEME_PRIMARY_ID = 1;
-short THEME_PRIMARY_BG = COLOR_BLUE;
-short THEME_PRIMARY_FG = COLOR_WHITE;
+short G_THEME_PRIMARY_ID = 1;
+short G_THEME_PRIMARY_BG = COLOR_BLUE;
+short G_THEME_PRIMARY_FG = COLOR_WHITE;
 
-short THEME_SECONDARY_ID = 2;
-short THEME_SECONDARY_BG = COLOR_RED;
-short THEME_SECONDARY_FG = COLOR_WHITE;
+short G_THEME_SECONDARY_ID = 2;
+short G_THEME_SECONDARY_BG = COLOR_RED;
+short G_THEME_SECONDARY_FG = COLOR_WHITE;
 
-short THEME_PRIMARY_INVERSE_ID = 3;
-short THEME_PRIMARY_INVERSE_BG = COLOR_WHITE;
-short THEME_PRIMARY_INVERSE_FG = COLOR_BLUE;
+short G_THEME_PRIMARY_INVERSE_ID = 3;
+short G_THEME_PRIMARY_INVERSE_BG = COLOR_WHITE;
+short G_THEME_PRIMARY_INVERSE_FG = COLOR_BLUE;
 
-short THEME_SECONDARY_INVERSE_ID = 4;
-short THEME_SECONDARY_INVERSE_BG = COLOR_WHITE;
-short THEME_SECONDARY_INVERSE_FG = COLOR_RED;
+short G_THEME_SECONDARY_INVERSE_ID = 4;
+short G_THEME_SECONDARY_INVERSE_BG = COLOR_WHITE;
+short G_THEME_SECONDARY_INVERSE_FG = COLOR_RED;
 
-int KEY_ESC = 27;
-int SCREEN_HEIGHT = 25;
-int SCREEN_WIDTH = 80;
+const int KEY_ESC = 27;
+int G_SCREEN_HEIGHT = 25;
+int G_SCREEN_WIDTH = 80;
 
-WINDOW* MENU_WINDOW = NULL;
-WINDOW* STATUS_WINDOW = NULL;
-WINDOW* BODY_WINDOW = NULL;
+WINDOW* G_MENU_WINDOW = NULL;
+WINDOW* G_STATUS_WINDOW = NULL;
+WINDOW* G_BODY_WINDOW = NULL;
 
 void initializeApplication(void);
 void finalizeApplication(void);
 void clearScreen(void);
-void drawMenuBar(void);
-void drawBodyWindow(void);
-void drawStatusBar(void);
+void initializeMenuBar(void);
+void initializeBodyWindow(void);
+void initializeStatusBar(void);
+void doMainLoop(void);
 
 int main (int argc, char* argv[]) {
 
-  initscr();
-
-  if (has_colors())
-  {
-    start_color();
-
-    init_pair(THEME_PRIMARY_ID, THEME_PRIMARY_FG, THEME_PRIMARY_BG);
-    init_pair(THEME_SECONDARY_ID, THEME_SECONDARY_FG, THEME_SECONDARY_BG);
-    init_pair(THEME_PRIMARY_INVERSE_ID, THEME_PRIMARY_INVERSE_FG, THEME_PRIMARY_INVERSE_BG);
-    init_pair(THEME_SECONDARY_INVERSE_ID, THEME_SECONDARY_INVERSE_FG, THEME_SECONDARY_INVERSE_BG);
-  }
-
   initializeApplication();
-
-  // do stuff
-
-  // wait around so we can see things
-  int ch = wgetch(BODY_WINDOW);
-
-  while (ch != KEY_ESC) {
-    ch = wgetch(BODY_WINDOW);
-  }
-
+  doMainLoop();
   finalizeApplication();
-
-  endwin();
 
   return EXIT_SUCCESS;
 
@@ -70,81 +48,101 @@ int main (int argc, char* argv[]) {
 void initializeApplication() {
   PDC_set_title("Hello");
 
-  SCREEN_HEIGHT = getmaxy(stdscr);
-  SCREEN_WIDTH = getmaxx(stdscr);
+  initscr();
 
-  MENU_WINDOW = newwin(1, SCREEN_WIDTH, 0, 0);
-  STATUS_WINDOW = newwin(1, SCREEN_WIDTH, (SCREEN_HEIGHT - 1), 0);
-  BODY_WINDOW = newwin((SCREEN_HEIGHT - 2), SCREEN_WIDTH, 1, 0);
+  if (has_colors())
+  {
+    start_color();
+
+    init_pair(G_THEME_PRIMARY_ID, G_THEME_PRIMARY_FG, G_THEME_PRIMARY_BG);
+    init_pair(G_THEME_SECONDARY_ID, G_THEME_SECONDARY_FG, G_THEME_SECONDARY_BG);
+    init_pair(G_THEME_PRIMARY_INVERSE_ID, G_THEME_PRIMARY_INVERSE_FG, G_THEME_PRIMARY_INVERSE_BG);
+    init_pair(G_THEME_SECONDARY_INVERSE_ID, G_THEME_SECONDARY_INVERSE_FG, G_THEME_SECONDARY_INVERSE_BG);
+  }
+
+  G_SCREEN_HEIGHT = getmaxy(stdscr);
+  G_SCREEN_WIDTH = getmaxx(stdscr);
+
+  G_MENU_WINDOW = newwin(1, G_SCREEN_WIDTH, 0, 0);
+  G_STATUS_WINDOW = newwin(1, G_SCREEN_WIDTH, (G_SCREEN_HEIGHT - 1), 0);
+  G_BODY_WINDOW = newwin((G_SCREEN_HEIGHT - 2), G_SCREEN_WIDTH, 1, 0);
 
   curs_set(0);
 
   clearScreen();
 
-  drawMenuBar();
-  drawStatusBar();
-
-  drawBodyWindow();
+  initializeMenuBar();
+  initializeBodyWindow();
+  initializeStatusBar();
 
   curs_set(1);
 }
 
 void finalizeApplication() {
-  delwin(BODY_WINDOW);
-  delwin(STATUS_WINDOW);
-  delwin(MENU_WINDOW);
+  delwin(G_BODY_WINDOW);
+  delwin(G_STATUS_WINDOW);
+  delwin(G_MENU_WINDOW);
 
   curs_set(1);
+
+  endwin();
 }
 
 void clearScreen() {
   if (has_colors())
   {
-    bkgd(COLOR_PAIR(THEME_PRIMARY_ID));
+    bkgd(COLOR_PAIR(G_THEME_PRIMARY_ID));
   }
 
   clear();
   refresh();
 }
 
-void drawMenuBar() {
+void initializeMenuBar() {
   if (has_colors())
   {
-    wbkgd(MENU_WINDOW, COLOR_PAIR(THEME_PRIMARY_INVERSE_ID));
+    wbkgd(G_MENU_WINDOW, COLOR_PAIR(G_THEME_PRIMARY_INVERSE_ID));
   }
 
-  wclear(MENU_WINDOW);
+  wclear(G_MENU_WINDOW);
 
-  wprintw(MENU_WINDOW, "Hi! version 0.01 \t File \t Edit \t Help");
+  wprintw(G_MENU_WINDOW, "Hi! version 0.01 \t File \t Edit \t Help");
 
-  wrefresh(MENU_WINDOW);
+  wrefresh(G_MENU_WINDOW);
 }
 
-void drawBodyWindow() {
+void initializeBodyWindow() {
   if (has_colors())
   {
-    wbkgd(BODY_WINDOW, COLOR_PAIR(THEME_PRIMARY_ID));
+    wbkgd(G_BODY_WINDOW, COLOR_PAIR(G_THEME_PRIMARY_ID));
   }
 
-  wsetscrreg(BODY_WINDOW, 1, (SCREEN_HEIGHT - 2));
-  scrollok(BODY_WINDOW, TRUE);
+  wsetscrreg(G_BODY_WINDOW, 1, (G_SCREEN_HEIGHT - 2));
+  scrollok(G_BODY_WINDOW, TRUE);
 
-  wclear(BODY_WINDOW);
+  wclear(G_BODY_WINDOW);
 
-  wmove(BODY_WINDOW, 0, 0);
+  wmove(G_BODY_WINDOW, 0, 0);
 
-  wrefresh(BODY_WINDOW);
+  wrefresh(G_BODY_WINDOW);
 }
 
-void drawStatusBar() {
+void initializeStatusBar() {
   if (has_colors())
   {
-    wbkgd(STATUS_WINDOW, COLOR_PAIR(THEME_PRIMARY_INVERSE_ID));
+    wbkgd(G_STATUS_WINDOW, COLOR_PAIR(G_THEME_PRIMARY_INVERSE_ID));
   }
 
-  wclear(STATUS_WINDOW);
+  wclear(G_STATUS_WINDOW);
 
-  wprintw(STATUS_WINDOW, "ready...");
+  wprintw(G_STATUS_WINDOW, "ready...");
 
-  wrefresh(STATUS_WINDOW);
+  wrefresh(G_STATUS_WINDOW);
+}
+
+void doMainLoop() {
+  int ch = wgetch(G_BODY_WINDOW);
+  while (ch != KEY_ESC) {
+    ch = wgetch(G_BODY_WINDOW);
+  }  
 }
